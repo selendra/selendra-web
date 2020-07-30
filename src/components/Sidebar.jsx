8d978044e8147f6eca593d410da4c10dc9bb4037
Piset Heang <piset2cambo@gmail.com>
2020-07-30 18:58:57 +0700

    init

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+
+class Sidebar extends Component {
+  state = {  }
+  render() {
+    return (
+      <div className="bg-white">
+        <ul className="px-4">
+          <li className="btn flex justify-end py-2">
+            <button className="font-bold">Home</button> 
+          </li>
+          <li className="btn flex justify-end py-2">
+            <button className="font-bold">About</button>
+          </li>
+          <li className="btn flex justify-end py-2">
+            <button className="font-bold">Privacy & Policy</button>
+          </li>
+          <li className="btn flex justify-end py-2">
+            <button className="font-bold">Term Of Use</button>
+          </li>
+          <li className="btn flex justify-end py-2">
+            <button className="font-bold">Blog</button>
+          </li>
+          <li className="btn text-orange-500 flex justify-end py-2">
+            <button className="font-bold">LogIn</button>
+          </li>
+          <li className="btn text-orange-500 rounded  flex justify-end py-2">
+            <button className="font-bold">Sign Up</button>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Sidebar;
