7b61e7b447d44591bb0db92e0b41e53684fb9099
Piset Heang <piset2cambo@gmail.com>
2020-09-14 15:40:41 +0700

    resposive

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,7 +9,7 @@ import { ReactComponent as Linkedin} from '../../assets/linkedin.svg';
 function Footer() {
   return (
     <Row className="footer">
-      <Col span={6}>
+      <Col xs={12} sm={12} md={6} lg={6} xl={6}>
         <p className='footer_title'>Company</p>
         <div className='footer_sub'>
           <p>About Selendra</p>
@@ -17,7 +17,7 @@ function Footer() {
           <p>Term of use</p>
         </div>
       </Col>
-      <Col span={6}>
+      <Col xs={12} sm={12} md={6} lg={6} xl={6}>
         <p className='footer_title'>Solution</p>
         <div className='footer_sub'>
           <p>Customer Loyalty</p>
@@ -25,30 +25,30 @@ function Footer() {
           <p>API for payment solution</p>
         </div>
       </Col>
-      <Col span={6}>
+      <Col xs={12} sm={12} md={6} lg={6} xl={6}>
         <p className='footer_title'>Developers</p>
         <div className='footer_sub'>
           <p>Documentation</p>
           <p>GitHub</p>
         </div>
       </Col>
-      <Col span={6}>
+      <Col xs={12} sm={12} md={6} lg={6} xl={6}>
         <p className='footer_title'>Connect with us</p>
         <Row>
           <Col span={12}>
-            <Facebook/>
+            <Facebook className='footer_social'/>
           </Col>
           <Col span={12}>
-            <Telegram/>
+            <Telegram className='footer_social'/>
           </Col>
         </Row>
         <div style={{padding: '1rem 0'}}></div>
         <Row>
           <Col span={12}>
-            <Medium/>
+            <Medium className='footer_social'/>
           </Col>
           <Col span={12}>
-            <Linkedin/>
+            <Linkedin className='footer_social'/>
           </Col>
         </Row>
       </Col>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,30 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Layout, Menu, Row, Col, Button } from 'antd';
 import '../../style/Nav.css';
 import logo from '../../assets/selendra.png';
 import { ReactComponent as Dropdown } from '../../assets/dropdown.svg';
+import { ReactComponent as MenuBar } from '../../assets/menu.svg';
 
 const { Header } = Layout;
+
 function Nav() {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  }
   return (
-    <Header>
-      <Menu theme="dark" mode="horizontal">
-        <Menu.Item key="1">
-          <img src={logo} alt="logo" className="logo"/>
-          <span className="title">selendra</span>
-        </Menu.Item>
-        <Menu.Item key="2">
-          <span>Company <Dropdown className="dropdown"/></span>
-        </Menu.Item>
-        <Menu.Item key="3">Product</Menu.Item>
-        <Menu.Item className='signup'>
-          <Button className="button">Sign Up</Button>
-        </Menu.Item>
-        <Menu.Item className='login'>
-          <span style={{color: '#08B952'}}>Login</span>
-        </Menu.Item>
-      </Menu>
-    </Header>
+    <>
+      <Header className='header'>
+        <Menu theme="dark" mode="horizontal">
+          <Menu.Item key="1">
+            <img src={logo} alt="logo" className="logo"/>
+            <span className="title">selendra</span>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <span>Company <Dropdown className="dropdown"/></span>
+          </Menu.Item>
+          <Menu.Item key="3">Product</Menu.Item>
+          <Menu.Item className='signup'>
+            <Button className="button">Sign Up</Button>
+          </Menu.Item>
+          <Menu.Item className='login'>
+            <span style={{color: '#08B952'}}>Login</span>
+          </Menu.Item>
+        </Menu>
+      </Header>
+      <div className='mobile_nav'>
+        <div className='mobile_header'>
+          <Row align='middle' style={{height: '80px'}}>
+            <MenuBar className='menu_icon' onClick={handleNav}/>
+            <Row justify='center' style={{width: '100vw', position: 'absolute'}}>
+            <img src={logo} alt="logo" className="logo"/>
+            <span className="title">selendra</span>
+            </Row>
+          </Row>
+        </div>
+      {
+        nav && 
+        <div className='navItem'>
+          <Row justify='center' style={{height: '100vh', width: '100vw'}}>
+          <ul>
+            <li>
+              <span>Home</span>
+            </li>
+            <li>
+              <span>Company</span>
+            </li>
+            <li>
+              <span>Products</span>
+            </li>
+            <li>
+              <span>About Us</span>
+            </li>
+            <li>
+              <span>Privacy & Policy</span>
+            </li>
+            <li>
+              <span>Login</span>
+            </li>
+            <li>
+              <Button className='button nav_btn' size='large'>Sign Up</Button>
+            </li>
+          </ul>
+          </Row>
+        </div>
+      }
+      </div>
+    </>
   )
 }
 
