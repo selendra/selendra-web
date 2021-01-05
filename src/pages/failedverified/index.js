d0232bc6777b1cbea1f7ec0c59a29b9e8c228b4f
Piset Heang <piset2cambo@gmail.com>
2021-01-05 16:03:15 +0700

    UI

--- src/pages/failedverified/index.js
+++ src/pages/failedverified/index.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import phone from '../../assets/phone-failed.png';
+import { Row, Col, Button } from 'antd';
+import './styles/failed.css'
 
 export default function Failedverified() {
   return (
-    <div>
-      
+    <div className='failed'>
+      <div className='failed__container'>
+        <Row justify='center' align='middle'>
+          <Col>
+            <img src={phone} alt='phone' />
+          </Col>
+          <Col>
+            <p className='failed__title'>Failed Verified</p>
+            <div className='failed__btn'>
+              <Button size='large'>
+                <a href='/'>Back</a>
+              </Button>
+            </div>
+          </Col>
+        </Row>
+      </div>
     </div>
   )
 }
