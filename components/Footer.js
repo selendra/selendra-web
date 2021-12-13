9bcafaacf4b82b368c7a6d8265c2f695a1c2f622
combo <combo@combos-MacBook-Pro.local>
2021-12-14 00:55:26 +0700

    fix broken link

--- components/Footer.js
+++ components/Footer.js
@@ -25,49 +25,49 @@ export default function Footer() {
             <h1>Developers</h1>
             <a href='https://docs.selendra.org'>Documentation</a>
             <a href='https://github.com/selendra'>GitHub</a>
-            <a href='/pdf/Selendra-Economic-Paper-v1.0.pdf' download>Whitepaper</a>
+            <a href='https://github.com/selendra/selendra-docs/blob/master/docs/Whitepaper/selendra-economic-paper-v1.3.pdf'>Whitepaper</a>
           </Col>
           <Col xs={12} sm={12} md={4} lg={3} xl={3}>
             <h1>Support</h1>
-            <Link href='/product'><p>FAQs</p></Link>
+            <Link href='/faqs'><p>FAQs</p></Link>
             {/* <Link href='/product'><p>Contact Us</p></Link> */}
-            <a href='https://t.me/selendraorg'><p>Telegram Community</p></a>
+            <a href='https://t.me/selendrachain'><p>Telegram Community</p></a>
           </Col>
           <Col xs={0} sm={0} md={4} lg={4} xl={4}>
             <Row justify='end'>
               <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-                <a href='https://www.facebook.com/selendraio'><img src='/images/facebook.png' alt='facebook'/></a>
+                <a href='https://www.facebook.com/selendrachain'><img src='/images/facebook.png' alt='facebook'/></a>
               </Col>
               <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-                <a href='https://twitter.com/selendraorg'><img src='/images/twitter.png' alt='twitter'/></a>
+                <a href='https://twitter.com/selendrachain'><img src='/images/twitter.png' alt='twitter'/></a>
               </Col>
               <Col xs={12} sm={12} md={12} lg={12} xl={12}>
                 <a href='https://medium.com/selendra'><img src='/images/medium.png' alt='medium'/></a>
               </Col>
               <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-                <a href='https://t.me/selendraorg'><img src='/images/telegram.png' alt='telegram'/></a>
+                <a href='https://t.me/selendrachain'><img src='/images/telegram.png' alt='telegram'/></a>
               </Col>
               <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-                <a href='https://www.linkedin.com/company/selendra'><img src='/images/linkedin.png' alt='linkedin'/></a>
+                <a href='https://www.linkedin.com/company/selendrachain'><img src='/images/linkedin.png' alt='linkedin'/></a>
               </Col>
             </Row>
           </Col>
           <Col xs={24} sm={24} md={0} lg={0} xl={0} style={{paddingTop: '20px'}}>
             <Row justify='center'>
               <Col xs={4} sm={4}>
-                <a href='https://www.facebook.com/selendraio'><img src='/images/facebook.png' alt='facebook'/></a>
+                <a href='https://www.facebook.com/selendrachain'><img src='/images/facebook.png' alt='facebook'/></a>
               </Col>
               <Col xs={4} sm={4}>
-                <a href='https://twitter.com/selendraorg'><img src='/images/twitter.png' alt='twitter'/></a>
+                <a href='https://twitter.com/selendrachain'><img src='/images/twitter.png' alt='twitter'/></a>
               </Col>
               <Col xs={4} sm={4}>
                 <a href='https://medium.com/selendra'><img src='/images/medium.png' alt='medium'/></a>
               </Col>
               <Col xs={4} sm={4}>
-                <a href='https://t.me/selendraorg'><img src='/images/telegram.png' alt='telegram'/></a>
+                <a href='https://t.me/selendrachain'><img src='/images/telegram.png' alt='telegram'/></a>
               </Col>
               <Col xs={4} sm={4}>
-                <a href='https://www.linkedin.com/company/selendra'><img src='/images/linkedin.png' alt='linkedin'/></a>
+                <a href='https://www.linkedin.com/company/selendrachain'><img src='/images/linkedin.png' alt='linkedin'/></a>
               </Col>
             </Row>
           </Col>
